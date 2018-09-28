183b372cbd35474d5f4fe837d15e1a4fa9689ce7
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-28 16:30:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("div#nested div.target");
+}
+
+function deepestChild() {
+  let list = document.querySelector("div#grand-node").querySelectorAll("div");
+  for (let i = 0; i < list.length; i++) {
+    if (!list[i].querySelector("div")) {
+      return list[i]
+    }
+  }
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll("ul.ranked-list li")
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
