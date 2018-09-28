6c4411adb72a89e24ba6b2dbdcc301b4f9d66dd5
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-28 16:27:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deepestChild() {
 function increaseRankBy(n) {
   let list = document.querySelectorAll("ul.ranked-list li")
   for (let i = 0; i < list.length; i++) {
-    list[i].innerHTML = ()
+    list[i].innerHTML = (list)
   }
 }
